16f1a97e38c7fc5de24d82d9924f36e8448a58b3
hrsk <hrsk.yegor@gmail.com>
2024-09-05 15:40:47 +0300

    react-kabzda: 00, 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            THIS IS APP COMPONENT!
+
+            <Accordion/>
+
+            <Rating/>
+        </div>
+    );
 }
 
 export default App;
+
+function Star() {
+    return <span>star </span>
+}
+
+function Rating() {
+    return (
+        <div>
+            <h1>Rating</h1>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    )
+}
+
+function Accordion() {
+    return (
+        <div>
+            <h1>
+                Menu
+            </h1>
+            <ul>
+                <li>item 1</li>
+                <li>item 2</li>
+                <li>item 3</li>
+                <li>item 4</li>
+            </ul>
+        </div>
+    )
+}
