c4546d9d827c116dcfc69149a5445a21476bb11c
hrsk <hrsk.yegor@gmail.com>
2024-09-10 18:19:35 +0300

    callbacks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Accordion} from "./components/Accordion";
-import {Rating} from "./components/Rating";
+import {Rating, RatingValueType, RatingWithCallbackValue, RatingWithPropsValue} from "./components/Rating";
 import {OnOff} from "./components/OnOff";
 import {UncontrolledAccordion} from "./components/UncontrolledAccordion";
 import {UncontrolledRating} from "./components/UncontrolledRating";
 
 function App() {
+
+    const [ratingValue, setRatingValue] = useState<RatingValueType>(0);
+
+
     return (
         <div className="App">
             <PageTitle title={'THIS IS APP COMPONENT!'}/>
@@ -16,19 +20,23 @@ function App() {
             <Accordion title={'Menu'} collapsed={false}/>
             <Accordion title={'Users'} collapsed={true}/>
 
-            <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
             <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
+            <RatingWithCallbackValue value={ratingValue} setRatingValue={setRatingValue}/>
+            <RatingWithPropsValue value={ratingValue} setRatingValue={setRatingValue}/>
+
+            {/*<Rating value={0}/>*/}
+            {/*<Rating value={1}/>*/}
+            {/*<Rating value={2}/>*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Rating value={4}/>*/}
+            {/*<Rating value={5}/>*/}
 
             {/*<OnOff on={true}/>*/}
             <OnOff/>
 
-            <UncontrolledAccordion title={'Uncontrolled MENU'} />
+            <UncontrolledAccordion title={'Uncontrolled MENU'}/>
 
-            <UncontrolledRating />
+            <UncontrolledRating/>
         </div>
     );
 }
--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -1,16 +1,105 @@
 import React from "react";
 
+type RatingWithCallbackValueType = {
+    value: RatingValueType
+    setRatingValue: (value: RatingValueType) => void
+}
+
+type StarWithCallbackValuePropsType = {
+    selected: boolean
+    ratingValueHandler: () => void
+}
+
+function StarWithCallbackValue(props: StarWithCallbackValuePropsType) {
+
+    console.log('Star rendered')
+
+    return (
+        <span onClick={props.ratingValueHandler}>
+            {
+                props.selected
+                    ? <b>star </b>
+                    : 'start '}
+        </span>
+    )
+}
+
+export function RatingWithCallbackValue(props: RatingWithCallbackValueType) {
+
+    console.log('Rating rendered')
+
+    return (
+        <div>
+            <StarWithCallbackValue selected={props.value > 0} ratingValueHandler={() => props.setRatingValue(1)}/>
+            <StarWithCallbackValue selected={props.value > 1} ratingValueHandler={() => props.setRatingValue(2)}/>
+            <StarWithCallbackValue selected={props.value > 2} ratingValueHandler={() => props.setRatingValue(3)}/>
+            <StarWithCallbackValue selected={props.value > 3} ratingValueHandler={() => props.setRatingValue(4)}/>
+            <StarWithCallbackValue selected={props.value > 4} ratingValueHandler={() => props.setRatingValue(5)}/>
+        </div>
+    )
+}
+
+type StarWithPropsValuePropsType = {
+    selected: boolean
+    value: RatingValueType
+    ratingValueHandler: (value: RatingValueType) => void
+}
+
+function StarWithPropsValue(props: StarWithPropsValuePropsType) {
+
+    console.log('Star rendered')
+
+    return (
+        <span onClick={() => props.ratingValueHandler(props.value)}>
+            {
+                props.selected
+                    ? <b>star </b>
+                    : 'start '}
+        </span>
+    )
+}
+
+
+type RatingWithPropsValuePropsType = {
+    value: RatingValueType
+    setRatingValue: (value: RatingValueType) => void
+}
+
+export function RatingWithPropsValue(props: RatingWithPropsValuePropsType) {
+
+    console.log('Rating rendered')
+
+    return (
+        <div>
+            <StarWithPropsValue selected={props.value > 0} value={1} ratingValueHandler={props.setRatingValue}/>
+            <StarWithPropsValue selected={props.value > 1} value={2} ratingValueHandler={props.setRatingValue}/>
+            <StarWithPropsValue selected={props.value > 2} value={3} ratingValueHandler={props.setRatingValue}/>
+            <StarWithPropsValue selected={props.value > 3} value={4} ratingValueHandler={props.setRatingValue}/>
+            <StarWithPropsValue selected={props.value > 4} value={5} ratingValueHandler={props.setRatingValue}/>
+        </div>
+    )
+}
+
 type StarPropsType = {
     selected: boolean
 }
+
 function Star(props: StarPropsType) {
 
     console.log('Star rendered')
 
-    return props.selected ? <span><b>star </b></span> : <span>star </span>
+    return (
+        <span>
+            {
+                props.selected
+                    ? <b>star </b>
+                    : 'start '}
+        </span>
+    )
 }
 
-type RatingValueType = 0 | 1 | 2 | 3 | 4 | 5
+export type RatingValueType = 0 | 1 | 2 | 3 | 4 | 5
+
 type RatingPropsType = {
     value: RatingValueType
 }
